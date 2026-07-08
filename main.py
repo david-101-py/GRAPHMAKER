87790338a83bd660c5729c7a78e33a382e453697
david_011_git <davidgonzalezsaudinos@gmail.com>
2026-07-08 20:37:05 +0200

    Empezando a pensar en la interfaz y el flujo aunque falta historial

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from data_service import create_db_file, create_serie
 from config_service import show_config, load_config
 
 def main():
-    create_serie("Example Serie", group="Example Group")
-    load_config()
-    show_config()
+    # Primero se tiene que cargar todo incluido la configuración y las bases de datos
+    # Luego se muestran las funciones disponibles y se ejecuta la función seleccionada
+    # Las funciones se verán de tal manera que se cree una interfaz de usuario en la terminal para que el usuario pueda interactuar con el programa 
+    # La distribución de la interfaz de usuario será la siguiente:
+    # Habrá un grupo de botones que representarán las funciones disponibles, y al seleccionar uno de ellos se mostrará un formulario para que el usuario pueda ingresar los datos necesarios para ejecutar la función seleccionada.
+    # También se mostrará un botón para volver al menú principal y otro para salir del programa (arriba a la izquierda y arriba a la derecha respectivamente)
+    
+    
+    pass
+
 if __name__ == "__main__":
     main()
